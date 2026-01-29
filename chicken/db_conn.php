a416e5d88d1ec20e07e8ba70a5033d8fc944b831
ParkSoyoung77 <39236790+ParkSoyoung77@users.noreply.github.com>
2026-01-29 15:46:56 +0900

    Update db_conn.php

--- chicken/db_conn.php
+++ chicken/db_conn.php
@@ -1,12 +1,15 @@
 <?php
-$host = "http://chickendb.c1yq4myeord7.ap-northeast-2.rds.amazonaws.com"; // RDS 엔드포인트
+$host = "chickendb.c1yq4myeord7.ap-northeast-2.rds.amazonaws.com"; // RDS 엔드포인트
 $user = "admin";
 $pw = "qwe12345";
 $dbName = "chickendb";
 
+
 $conn = mysqli_connect($host, $user, $pw, $dbName);
+$sql ="SELECT * FROM Megazone_Chicken WHERE userID='".$_GET['userID']."'";
 
 if (!$conn) {
     die("연결 실패: " . mysqli_connect_error());
 }
+
 ?>
